ad9126e17e984e7b1bafe54601dc4aecc0705923
paras603 <parasmanirai1@gmail.com>
2025-11-26 14:30:22 -0500

    latest

--- app/Http/Controllers/ConnectionController.php
+++ app/Http/Controllers/ConnectionController.php
@@ -9,28 +9,43 @@ use Illuminate\Http\Request;
 class ConnectionController extends Controller
 {
     use HttpResponses;
+//    public function follow(Request $request, $user_id)
+//    {
+//        $user = auth()->user();
+//
+//        if($user->id == $user_id){
+//            return $this->error(null, "You can't follow yourself.", 422);
+//        }
+//
+//        $user->following()->syncWithoutDetaching([$user_id]);
+//
+//        return $this->success(['user' => $user_id, 'follows' => $user->id], "Followed.", 200);
+//    }
+//
+//    public function unfollow(Request $request, $user_id)
+//    {
+//        $user = auth()->user();
+//
+//        $user->following()->detach([$user_id]);
+//
+//        return $this->success(null, "Unfollowed.", 200);
+//    }
+
     public function follow(Request $request, $user_id)
     {
-        $user = auth()->user();
+        $auth_user = auth()->user();
+        $following_id = $user_id;
 
-        if($user->id == $user_id){
-            return $this->error(null, "You can't follow yourself.", 422);
+        if($auth_user->id == $following_id) {
+            return $this->error(null,'You cannot follow yourself.', 422);
         }
 
-        $user->following()->syncWithoutDetaching([$user_id]);
+        $auth_user->following()->toggle($following_id);
 
-        return $this->success(['user' => $user_id, 'follows' => $user->id], "Followed.", 200);
+        $auth_user->following()->toggle($following_id);
+
+        $auth_user->following()->
     }
-
-    public function unfollow(Request $request, $user_id)
-    {
-        $user = auth()->user();
-
-        $user->following()->detach([$user_id]);
-
-        return $this->success(null, "Unfollowed.", 200);
-    }
-
     public function followers()
     {
         $user = auth()->user();
